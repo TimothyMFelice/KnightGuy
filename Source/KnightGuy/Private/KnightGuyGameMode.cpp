9e33fe035a07677aa525f7fe1993e4f5bfd64148
TimothyMFelice <TimothyMFelice@gmail.com>
2020-03-19 19:54:43 -0700

    Linter

--- Source/KnightGuy/Private/KnightGuyGameMode.cpp
+++ Source/KnightGuy/Private/KnightGuyGameMode.cpp
@@ -3,12 +3,13 @@
 
 #include "KnightGuyGameMode.h"
 #include "UObject/ConstructorHelpers.h"
+#include "Characters/KightGuyCharacter.h"
 
 
 AKnightGuyGameMode::AKnightGuyGameMode(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> KnightGuyCharacterBPClass(TEXT("/Game/KnightGuy/Core/Characters/BP_KnightGuyCharacter.BP_KnightGuyCharacter"));
+	static ConstructorHelpers::FClassFinder<AKightGuyCharacter> KnightGuyCharacterBPClass(TEXT("/Game/KnightGuy/Core/Characters/Blueprints/BP_KnightGuyCharacter"));
 	if (KnightGuyCharacterBPClass.Class != NULL)
 	{
 		DefaultPawnClass = KnightGuyCharacterBPClass.Class;
